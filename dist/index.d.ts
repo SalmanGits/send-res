3c35bec9b932f5a681134a065021d728e8401772
SalmanGits <belalahmad10310@gmail.com>
2025-02-22 20:04:19 +0530

    ..

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Send response with status and additional data.
- * @param {object} params - Object containing response, status, and additional data.
- * @param {Express.Response} params.res - Express response object.
- * @param {number} params.status - HTTP status code.
- * @param {object} params.data - Additional data to send in the response.
- */
-import { Response } from 'express';
-export declare function sendResponse<T>(res: Response, status: number, data?: T): void;
